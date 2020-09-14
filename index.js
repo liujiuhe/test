c37f1cca7d7bd528cb672cc7a1f8f189cc8a48a7
刘家赫 <2537546514@qq.com>
2020-09-14 10:44:42 +0800

    添加了双重for循环

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var div = document.querySelector('div');
 div.onclick = function () {
   alert('哈哈哈哈哈');
 };
+var num = 0;
 for (var i = 0; i < 100; i++) {
-  i = i * i;
-  div.innerHTML = i
+  for (var j = 0; j < 100; j++) {
+    num = i * j;
+    div.innerHTML = num;
+  }
 }
